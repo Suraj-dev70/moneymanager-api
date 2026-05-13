d5de272d6ef0e50d560df49084677f804836984d
Suraj-dev70 <surajofficial7071@gmail.com>
2026-05-13 16:05:36 +0530

    CH

--- src/main/java/com/moneymanagerproject/config/SecurityConfig.java
+++ src/main/java/com/moneymanagerproject/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,25 +29,22 @@ public class SecurityConfig {
     public final AppUserDetailService appUserDetailService;
     public final JwtFilter jwtFilter;
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         return http
                 .csrf(csrf -> csrf.disable())
                 .cors(Customizer.withDefaults())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-
                                 "/register",
-
                                 "/status",
-
                                 "/login",
-
                                 "/activate",
-
                                 "/health"
-
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
