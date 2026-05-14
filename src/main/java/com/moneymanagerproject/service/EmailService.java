b28f6130f4dc40bfc1ce0f20a9632f108b015e9f
Suraj-dev70 <surajofficial7071@gmail.com>
2026-05-14 06:30:34 +0530

    ASYNC

--- src/main/java/com/moneymanagerproject/service/EmailService.java
+++ src/main/java/com/moneymanagerproject/service/EmailService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +17,7 @@ public class EmailService {
 
     @Value("${spring.mail.properties.mail.smtp.from}")
     private String fromEmail;
-
+    @Async
     public void sendSimpleMail(String to, String subject, String text) {
         try{
             SimpleMailMessage message = new SimpleMailMessage();
